1201a9136b8b083ad9223902c10d14485aaeecfa
Ariele Mendonça <ariele_apm@outlook.com>
2021-04-22 22:17:43 -0300

    Separa o componente Content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
 // import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -81,14 +81,7 @@ export function App() {
         <header>
           <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
         </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+        <Content selectedGenre={selectedGenreId} movies={movies}/>
       </div>
     </div>
   )
